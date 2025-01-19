1f9173dc2be6253b72415ddbf127b6b7013be968
Romain Tirbisch <romain_tirbisch@yahoo.com>
2025-01-19 08:49:27 +0100

    Initial Commit

---
+++ SwiftUI_GraphicsAndAnimations/SwiftUI_GraphicsAndAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GraphicsAndAnimationsApp.swift
+//  SwiftUI_GraphicsAndAnimations
+//
+//  Created by Elfo on 19/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GraphicsAndAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
